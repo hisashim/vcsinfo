bd460e19196bb27d1bedcf54bed5305018563e54
Hisashi Morita <hisashim@workbook.org>
2012-05-11 02:40:38 +0900

    More changelog-like output

--- vcslog.sh
+++ vcslog.sh
@@ -17,7 +17,7 @@ which git >/dev/null; [ x"$?" = x0 ] && GIT=TRUE
 if [ x"${GIT}" = xTRUE ]; then
   (cd "${WD}" && git status --porcelain >/dev/null 2>&1) && GIT_WD=TRUE
   if [ x"${GIT_WD}" = xTRUE ]; then
-    (cd "${WD}"; git log | cat)
+    (cd "${WD}"; git --no-pager log --format="%ai %aN %n%n%x09* %s%n")
   fi
 fi
 
@@ -26,7 +26,7 @@ which hg >/dev/null; [ x"$?" = x0 ] && HG=TRUE
 if [ x"${HG}" = xTRUE ]; then
   (hg status "${WD}" >/dev/null 2>&1) && HG_WD=TRUE
   if [ x"${HG_WD}" = xTRUE ]; then
-    (cd "${WD}"; hg log --rev tip:0)
+    (cd "${WD}"; hg log --style changelog)
   fi
 fi
 
@@ -40,10 +40,15 @@ if [ x"${BZR}" = xTRUE ]; then
 fi
 
 # Subversion
-which svn >/dev/null; [ x"$?" = x0 ] && SVN=TRUE
+which svn    >/dev/null; [ x"$?" = x0 ] && SVN=TRUE
+which svn2cl >/dev/null; [ x"$?" = x0 ] && SVN2CL=TRUE
 if [ x"${SVN}" = xTRUE ]; then
   (svn info "${WD}" >/dev/null 2>&1) && SVN_WD=TRUE
   if [ x"${SVN_WD}" = xTRUE ]; then
-    (cd "${WD}"; svn log -rBASE:0 -v)
+    if [ x"${SVN2CL}" = xTRUE ]; then
+      (cd "${WD}"; svn2cl --stdout)
+    else
+      (cd "${WD}"; svn log -rBASE:0 -v)
+    fi
   fi
 fi
